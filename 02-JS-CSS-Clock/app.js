d293e8587f2ea1848f75c532e39419193f93de33
MoSaif00 <moh14ab@gmail.com>
2020-12-11 22:58:03 +0100

    finished the chanllenge now adding digital clock and control the transation when it reach 360degree

---
+++ 02-JS-CSS-Clock/app.js
@@ -0,0 +1,17 @@
+const secondHand = document.querySelector('.second-hand');
+const minuteHand = document.querySelector('.min-hand');
+const hourHand = document.querySelector('.hour-hand');
+
+function setDate() {
+  const now = new Date();
+  const seconds = now.getSeconds();
+  const secondsDegrees = (seconds / 60) * 360 + 90;
+  secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+  const minutes = now.getMinutes();
+  const minutesDegrees = (minutes / 60) * 360 + 90;
+  minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
+  const hour = now.getHours();
+  const hoursDegrees = (hour / 12) * 360 + 90;
+  hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
+}
+setInterval(setDate, 1000);
