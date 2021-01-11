fc2a3751ceef5104d51bffe142de067dca90d49e
MoSaif00 <moh14ab@gmail.com>
2021-01-11 18:35:15 +0100

    finished last challenge

--- 30-Whack-A-Mole-Game/app.js
+++ 30-Whack-A-Mole-Game/app.js
@@ -1,3 +1,45 @@
 const holes = document.querySelectorAll('.hole');
 const scoreBoard = document.querySelector('.score');
 const moles = document.querySelectorAll('.mole');
+const startbtn = document.querySelector('.start-btn');
+let lastHole;
+let timeUp = false;
+let score = 0;
+function randomTime(min, max) {
+  return Math.round(Math.random() * (max - min) - min);
+}
+function randomHole(holes) {
+  const idx = Math.floor(Math.random() * holes.length);
+  const hole = holes[idx];
+  if (hole === lastHole) {
+    return randomHole(holes);
+  }
+  lastHole = hole;
+  return hole;
+}
+
+function peep() {
+  const time = randomTime(300, 1100);
+  const hole = randomHole(holes);
+  hole.classList.add('up');
+  setTimeout(() => {
+    hole.classList.remove('up');
+    if (!timeUp) peep();
+  }, time);
+}
+
+function bonk(e) {
+  if (!e.isTrusted) return;
+  score++;
+  this.classList.remove('up');
+  scoreBoard.textContent = score;
+}
+startbtn.addEventListener('click', function () {
+  scoreBoard.textContent = 0;
+  timeUp = false;
+  score = 0;
+  peep();
+  setTimeout(() => (timeUp = true), 15000);
+});
+
+moles.forEach((mole) => mole.addEventListener('click', bonk));
