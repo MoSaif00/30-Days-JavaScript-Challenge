2185e4d76e85324f6745c59c4fc32ec3d9a3399f
MoSaif00 <moh14ab@gmail.com>
2021-01-16 22:06:49 +0100

    updated the images and titles of each challenge in the main demo

--- 25-EventCapture-Propagation-BubblingAndOnce/app.js
+++ 25-EventCapture-Propagation-BubblingAndOnce/app.js
@@ -2,7 +2,7 @@ const divs = document.querySelectorAll('div');
 
 function logText(e) {
   console.log(this.classList.value);
-  //   e.stopPropagation();
+  // e.stopPropagation();
 }
 
 divs.forEach((div) =>
